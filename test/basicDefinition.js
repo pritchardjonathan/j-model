20e446394224f6899479753784a6a055c7813f0f
Jonathan Pritchard <jon@node28.com>
2014-12-11 00:01:16 +0000

    Added test for passing data into a models constructor

--- test/basicDefinition.js
+++ test/basicDefinition.js
@@ -18,4 +18,28 @@ describe("Basic definition", function() {
 
     assert.equal(typeof u1.name, "number");
   });
+  it("Should accept and convert attribute data passed in the model constructor", function () {
+    var User = jModel.create("User", {
+      attributes:[
+        { name: "name", type: String },
+        { name: "age", type: Number },
+        { name: "dateOfBirth", type: Date },
+        { name: "admin", type: Boolean },
+        { name: "roles", type: [ String ] }
+      ]
+    }),
+      u1 = new User({
+        name: 1,
+        age: "100",
+        dateOfBirth: "2013-02-08 09:30:26.123",
+        admin: "true",
+        roles:[ 1, new Date(0), true ]
+      });
+
+    assert.equal(u1.name, 1);
+    assert.equal(u1.age, 100);
+    assert.equal(u1.dateOfBirth, "1");
+    assert.equal(u1.admin, true);
+    assert.equal(u1.roles, [ "1", "", "true" ]);
+  });
 });
