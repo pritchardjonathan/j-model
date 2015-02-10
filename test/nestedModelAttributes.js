3073c3bf7953b11b89941155a1a9cc886c543e45
Jonathan Pritchard <jon@node28.com>
2015-02-10 22:42:06 +0000

    Fixed bug converting nested model array items

--- test/nestedModelAttributes.js
+++ test/nestedModelAttributes.js
@@ -15,7 +15,8 @@ describe("Nested model attributes", function() {
       User = jModel.create("User", {
         attributes: [
           { name: "roles", type: [ String ] },
-          { name: "address", type: Address }
+          { name: "address", type: Address },
+          { name: "previousAddresses", type: [ Address ]}
         ]
       }),
       u1 = new User({ address:{
@@ -24,6 +25,36 @@ describe("Nested model attributes", function() {
 
     assert.equal(u1.address.firstLine, "123 Somelane");
     assert.equal(u1.address.jModel.getName(), "Address");
+
+
+
+  });
+
+  it("Should accept multiple nested model types", function () {
+    var Address = jModel.create("Address", {
+        attributes: [
+          { name: "firstLine", type: String },
+          { name: "secondLine", type: String },
+          { name: "city", type: String },
+          { name: "postcode", type: String }
+        ]
+      }),
+      User = jModel.create("User", {
+        attributes: [
+          { name: "roles", type: [ String ] },
+          { name: "previousAddresses", type: [ Address ]}
+        ],
+        init: function(){
+          this.previousAddresses = [];
+        }
+      }),
+      u1 = new User();
+
+    u1.previousAddresses.push({ firstLine: "456 Anotherville" });
+
+    assert.equal(u1.previousAddresses.length, 1);
+    assert.equal(u1.previousAddresses[0].firstLine, "456 Anotherville")
+
   });
 
 
